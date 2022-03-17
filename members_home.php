e7b43c33bf3c28bf20dbd89a5ff82913c68485e3
Amititeloaie Cristian <78822218+AnataBakka@users.noreply.github.com>
2022-03-17 22:22:23 +0200

    original file

--- members_home.php
+++ members_home.php
@@ -1,5 +1,8 @@
-<?php
-echo "<h1>Members homepage</h1>";
-// developed by Adam MacKay 2000418 - 14/03/22
-
+<?php
+
+echo "<h1>Members homepage</h1>";
+
+// developed by Adam MacKay 2000418 - 14/03/22
+
+
 ?>
