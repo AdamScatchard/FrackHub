d67b05c773d78e24825dad8de5d6243122eb4f30
Amititeloaie Cristian <78822218+AnataBakka@users.noreply.github.com>
2022-03-17 22:01:41 +0200

    fixing github spacing issues

--- inc/menu.php
+++ inc/menu.php
@@ -1,21 +1,34 @@
-<div class="navbar">
-	<div class="logo_div">
-		<a href="index.php"><h1>FrackHub</h1></a>
-	</div>
-	<ul>
-	  <?php 	
-		if (isset($uid)){
-			echo "<li><a class=\"active\" href=\"?page=account\">Account</a>";
-			echo "<li><a class=\"active\" href=\"?page=advertise\">Advertise</a>";
-
-		}else{
-		    echo "<li><a class=\"active\" href=\"index.php\">Home</a>";
-		}
-		?>
-	  </a></li>
-	  <li><a href="#news">Search</a></li>
-	  <li><a href="#news">Message</a></li>
-	  <li><a href="#contact">Contact</a></li>
-	  <li><a href="?page=logout">Logout</a></li>
-	</ul>
+<div class="navbar">
+
+	<div class="logo_div">
+
+		<a href="index.php"><h1>FrackHub</h1></a>
+
+	</div>
+	
+	<ul>
+
+	<?php 	
+		if (isset($uid)){
+			echo '<li><a class="active" href="?page=account">Account</a></li>
+			<li><a href="?page=book_item">Book Item</a></li>';
+		}else{
+			echo '<li><a class="active" href="index.php">Home</a></li>';
+		}
+	?>
+
+	<li><a href="#news">Search</a></li>
+
+	<li><a href="#news">Message</a></li>
+
+	<li><a href="#contact">Contact</a></li>
+
+	<?php
+		if (isset($uid)){
+			echo '<li><a href="?page=logout">Logout</a></li>';
+		}
+	?>
+
+	</ul>
+
 </div>
