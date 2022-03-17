22072fdc98583ceff4a676a9a7c7dc5114d10640
Amititeloaie Cristian <78822218+AnataBakka@users.noreply.github.com>
2022-03-17 22:12:55 +0200

    original file

--- inc/footer.php
+++ inc/footer.php
@@ -1,1 +1,6 @@
-<?php	echo "<div class=\"footer\">";	echo "-- FrackHub Collaborative Development 5CS024 -- &copy;" . date('Y') . "";	echo "<div>";	echo "</html>";?>
+<?php
+	echo "<div class=\"footer\">";
+	echo "-- FrackHub Collaborative Development 5CS024 -- &copy;" . date('Y') . "";
+	echo "<div>";
+	echo "</html>"; 
+?>
