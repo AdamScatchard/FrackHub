04d48122e04b9a6caadbcd5be872a192db4acebb
Amititeloaie Cristian <78822218+AnataBakka@users.noreply.github.com>
2022-03-17 22:02:56 +0200

    Update no_db.php

--- inc/no_db.php
+++ inc/no_db.php
@@ -1,4 +1,6 @@
-<?php
-echo "<h1>Contact System Support</h1>";
-echo "<p>There is an issue connecting to the database, please verify the credentials.</p>";
+<?php
+
+echo "<h1>Contact System Support</h1>";
+
+echo "<p>There is an issue connecting to the database, please verify the credentials.</p>";
 ?>
