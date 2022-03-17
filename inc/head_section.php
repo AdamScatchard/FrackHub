941c5464d2347ddefc78564069a33b95a034eb7d
Amititeloaie Cristian <78822218+AnataBakka@users.noreply.github.com>
2022-03-17 22:13:54 +0200

    original file

--- inc/head_section.php
+++ inc/head_section.php
@@ -1,5 +1,19 @@
-<?php	echo "<!DOCTYPE html>";
-	echo "<html>";
-	echo "<head>";
-	echo "<link href=\"https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine\" rel=\"stylesheet\">";	// modified by adam mackay as it should have $css_dir define the css path.	// in the future the HTML title tag will contain data to show the page title.
-	echo "<link rel=\"stylesheet\" href=\"" . $css_dir . "page_formatting.css\">";	echo "<meta charset=\"UTF-8\">";	// page title needs adding here	echo "<title>This should be update by PHP</title>";	echo "</head>";	echo "<body>";?>
+<?php
+	echo "<!DOCTYPE html>";
+
+	echo "<html>";
+
+	echo "<head>";
+
+	echo "<link href=\"https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine\" rel=\"stylesheet\">";
+	// modified by adam mackay as it should have $css_dir define the css path.
+	// in the future the HTML title tag will contain data to show the page title.
+
+	echo "<link rel=\"stylesheet\" href=\"" . $css_dir . "page_formatting.css\">";
+	echo "<meta charset=\"UTF-8\">";
+
+	// page title needs adding here
+	echo "<title>This should be update by PHP</title>";
+	echo "</head>";
+	echo "<body>"; 
+?>
