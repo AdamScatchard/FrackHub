70544a69225b56bcc4dd46329e2f8f27d130595d
Amititeloaie Cristian <78822218+AnataBakka@users.noreply.github.com>
2022-03-17 22:21:49 +0200

    original file

--- logout.php
+++ logout.php
@@ -1,8 +1,14 @@
-<?php
-echo "<h1>Logout</h1>";
-if (isset($_COOKIE["frachub"])){
-	setcookie("frachub", "", time()-3600);
-}
-// developed by Adam MacKay 2000418 - 14/03/22
-
+<?php
+
+echo "<h1>Logout</h1>";
+
+if (isset($_COOKIE["frachub"])){
+
+	setcookie("frachub", "", time()-3600);
+
+}
+
+// developed by Adam MacKay 2000418 - 14/03/22
+
+
 ?>
