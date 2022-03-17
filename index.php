ee3c6c0970bc07ce5c09df45e6da37295cb26013
Amititeloaie Cristian <78822218+AnataBakka@users.noreply.github.com>
2022-03-17 22:18:52 +0200

    original file

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
-<?php 
-	// Adam Mackay's work
-	include ("page_init.php");
-	//Kieran C's Work
-	require_once($inc_dir . 'head_section.php');
-	
-	include ($inc_dir . 'menu.php');
-
-	include ($visiting_page);
-	
-	include($inc_dir . 'footer.php');
-	$db->disconnect();
-	
-?>
+<?php 
+
+	// Adam Mackay's work
+
+	include ("page_init.php");
+
+	//Kieran C's Work
+
+	require_once($inc_dir . 'head_section.php');
+
+	
+
+	include ($inc_dir . 'menu.php');
+
+
+
+	include ($visiting_page);
+
+	
+
+	include($inc_dir . 'footer.php');
+
+	$db->disconnect();
+
+	
+
+?>
