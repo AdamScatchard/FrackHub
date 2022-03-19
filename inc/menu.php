1c25e0c0e32d40b986bb32add48704a4f1addd91
Amititeloaie Cristian <78822218+AnataBakka@users.noreply.github.com>
2022-03-19 05:11:30 +0200

    hiding logout for non users and adding booking link + small formatting

--- inc/menu.php
+++ inc/menu.php
@@ -5,36 +5,30 @@
 		<a href="index.php"><h1>FrackHub</h1></a>
 
 	</div>
-
+	
 	<ul>
 
-	  <?php 	
-
+	<?php 	
 		if (isset($uid)){
-
-			echo "<li><a class=\"active\" href=\"?page=account\">Account</a>";
-
-			echo "<li><a class=\"active\" href=\"?page=advertise\">Advertise</a>";
-
-
-
+			echo '<li><a class="active" href="?page=account">Account</a></li>
+			<li><a class="active" href="?page=advertise">Advertise</a></li>
+			<li><a class="active" href="?page=book_item">Book Item</a></li>';
 		}else{
-
-		    echo "<li><a class=\"active\" href=\"index.php\">Home</a>";
-
+			echo '<li><a class="active" href="index.php">Home</a></li>';
 		}
+	?>
 
-		?>
+	<li><a href="#news">Search</a></li>
 
-	  </a></li>
+	<li><a href="#news">Message</a></li>
 
-	  <li><a href="#news">Search</a></li>
+	<li><a href="#contact">Contact</a></li>
 
-	  <li><a href="#news">Message</a></li>
-
-	  <li><a href="#contact">Contact</a></li>
-
-	  <li><a href="?page=logout">Logout</a></li>
+	<?php
+		if (isset($uid)){
+			echo '<li><a href="?page=logout">Logout</a></li>';
+		}
+	?>
 
 	</ul>
 
