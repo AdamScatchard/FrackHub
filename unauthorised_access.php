ad8cc79f7422a54085f84da0a68533ebbb5c67e5
Amititeloaie Cristian <78822218+AnataBakka@users.noreply.github.com>
2022-03-22 21:11:18 +0200

    original file

---
+++ unauthorised_access.php
@@ -0,0 +1,11 @@
+<h1>
+    Unauthorised Access
+</h1>
+<p>Please check the following...
+<ul>
+    <li>Cookies are unabled on this browser</li>
+    <li>You have logged in</li>
+    <li>Your credentials allow you access to this page normally</li>
+</ul><br>
+Where the problem persists, please contact technical support<br>
+</p>
