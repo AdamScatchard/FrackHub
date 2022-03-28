cdbd0b9ef40cc595587c1c4e597ed8b4bda20451
Amititeloaie Cristian <78822218+AnataBakka@users.noreply.github.com>
2022-03-28 08:58:09 +0300

    added redirects

--- logout.php
+++ logout.php
@@ -1,15 +1,12 @@
 <?php
-
-echo "<h1>Logout</h1>";
-
-if (isset($_COOKIE[$login_cookie])){
+	if (!isset($_COOKIE[$login_cookie])){
+		header("location:?page=unauthorised_access");
+		exit();
+	}
 
 	setcookie($login_cookie, "", time()-3600);
-
-}
+	header("location:?page=home_page");
+	exit();
 
 // developed by Adam MacKay 2000418 - 14/03/22
-
-
-
 ?>
