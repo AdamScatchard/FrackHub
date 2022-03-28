2345924d0c4cb2b8c5bfd0062dfcfff05f4ef908
Amititeloaie Cristian <78822218+AnataBakka@users.noreply.github.com>
2022-03-28 08:56:25 +0300

    invalid access redirect, formatting + form saved values

--- advertise.php
+++ advertise.php
@@ -1,78 +1,72 @@
 <?php
+	if (!isset($uid)){
+		header("location:?page=unauthorised_access");
+		exit();
+	}
 
-// developed by Adam MacKay 2000418 - 16/03/22
+	echo "<h1>Advertise Item</h1>";
 
+	echo "<br>";
 
+	if (isset($_POST['register'])){
 
-echo "<h1>Advertise Item</h1>";
+		// create database values to store in the database
 
-echo "<br>";
+		foreach ($_POST as $key => $value){
 
-if (isset($_POST['register'])){
+			if ($key != "register"){
 
-	// create database values to store in the database
+				$database_values[$key] = $value;	
 
-	foreach ($_POST as $key => $value){
-
-		if ($key != "register"){
-
-			$database_values[$key] = $value;	
+			}
 
 		}
 
+		$database_values['userID'] = $uid;
+		
+		//moderators are supposed to turn the active to true, but for testing purposes it's already turned here instead
+		$database_values['active'] = 1;
+
+		$database_values['available'] = 1;
+
+		$database_values['timestamp'] = time();				// timestamp of registration
+
+		$saved = $db->insert("fh_adverts", $database_values);	
+
+		if ($saved){
+			//forward to relevant confirmation page
+
+			echo ("Saved");
+
+		}
+		else{
+			echo ("Unable to register, try again or speak with administration");
+		}
 	}
-
-	$database_values['userID'] = $uid;
-	
-	//moderators are supposed to turn the active to true, but for testing purposes it's already turned here instead
-	$database_values['active'] = 1;
-
-	$database_values['available'] = 1;
-
-	$database_values['timestamp'] = time();				// timestamp of registration
-
-	
-
-	$saved = $db->insert("fh_adverts", $database_values);	
-
-	if ($saved){
-
-		//forward to relevant confirmation page
-
-		echo ("Saved");
-
-	}else{
-
-		echo ("Unable to register, try again or speak with administration");
-
-		// developers to repopulate the values into the HTML textbox values
-
-	}
-
-}
-
 ?>
 
-<form method="post" action="?page=advertise" name="submit_form" id="register_form" class="form">
+<?php
+	echo '<form method="post" action="?page=advertise" name="submit_form" id="register_form" class="form">
 
 	<p class="form_p">Title:</p>
 
-	<input type="text" id="name" name="name" placeholder="Title" class="form_txtBox">
+	<input type="text" id="name" name="name"' . (isset($_POST["name"])? ' value = "' . $_POST["name"] . '"' : "") . ' placeholder="Title" class="form_txtBox">
 
 	<p class="form_p">Description:</p>
 
-	<textarea id="name" name="description" placeholder="Advert" class="form_txtBox"></textarea>
+	<textarea id="name" name="description"' . (isset($_POST["description"])? ' value = "' . $_POST["description"] . '"' : "") . ' placeholder="Advert" class="form_txtBox"></textarea>
 
 	<p class="form_p">Items Available:</p>
 
-	<input type = "number" name="amount_available" id = "no_of_items" placeholder = "Number of items" required min = "1" class="form_txtBox">
+	<input type = "number" name="amount_available" id = "no_of_items"' . (isset($_POST["amount_available"])? ' value = "' . $_POST["amount_available"] . '"' : "") . ' placeholder = "Number of items" required min = "1" class="form_txtBox">
 	
 	<p class="form_p">Cost per day:</p>
 
-	<input type = "number" name="credits" id = "no_of_items" placeholder = "Number of credits" required min = "1" class="form_txtBox">
+	<input type = "number" name="credits" id = "no_of_credits"' . (isset($_POST["credits"])? ' value = "' . $_POST["credits"] . '"' : "") . ' placeholder = "Number of credits" required min = "1" class="form_txtBox">
 
 	<input type="submit" name="register" class="form_button" id="reg_button" value="Create Advert">
 
 	<input type="reset" name="clear" class="form_button" id="clear_button" value="Clear">
 
-</form>
+	</form>'
+?>
