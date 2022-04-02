33758dd2c7d15ca5ba962f7705a5f7eed9627628
WLVCollabTeam1 <99879568+WLVCollabTeam1@users.noreply.github.com>
2022-04-02 21:34:35 +0100

    Update page_init.php
    Modifications made to the user security, stopping users logging in and changing the logged in account by manipulating the cookie

--- page_init.php
+++ page_init.php
@@ -10,67 +10,87 @@
 	$encryption = new encryption();
 	$encryption->setKey($encryption_key);
 	$connected = $db->connect();	
+    function verify_access($db, $enc){
+        if(isset($_COOKIE[$GLOBALS['login_cookie']])){
+            if (isset($_COOKIE[$GLOBALS['session_code']])){
+				$user_data = $db->getRow("fh_users", "id='" . $_COOKIE[$GLOBALS['login_cookie']] . "'");
+                $salted_key = $user_data['id'] . $user_data['password'] . $user_data['lastlogin_timestamp'] . $user_data['timestamp'];
+				$enc->setPlainText(trim($salted_key));       
+                $session_Key = $enc->classRun();
+                $sessionCode = $GLOBALS['session_code'];
+                $cookie_code = $_COOKIE[$GLOBALS['session_code']];
+                if ($session_Key == $cookie_code){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 	if ($connected){
-		// check for existing login:
-		if (isset($_COOKIE[$login_cookie])){
-			$uid = $_COOKIE[$login_cookie];
-			if ($uid > 0){
-    			// reset cookie length
-				setcookie($login_cookie, $uid, $cookie_time);
-				$user = $db->getRow("fh_users", "id = '" . $uid . "'");
-				$userAccessLevel = $user['priviledge_id'];
-		        $accessCheck = $db->getRow("fh_priviledge_settings", "priviledge_id = '". $userAccessLevel . "'");
-		        if (isset($_GET['page']) && is_array($accessCheck)){
-    		        if (array_key_exists($_GET['page'], $accessCheck)){
-        	       		// check priviledges to see if user can have this page shown or not
-                		// if not divert away to home page (defined in the settings)
-                		if ($_GET['page'] != "logout"){
-                		    if ($accessCheck[$_GET['page']] == 1){
-                		       //Authorised
-                    			$visiting_page = $_GET['page'] . ".php";
-                		    }else{
-                		       // Unauthorised 
-                		       $visiting_page = "unauthorised_access.php";
-                		    }
-                    	}else{
-                			$visiting_page = $home_page;
-                		}
-    		        }else{
-    		            if ($_GET['page'] != "logout"){
-        		            // page doesnt exist
-                			$visiting_page = "404.php";
-        		        }else{
-        		            $visiting_page = "logout.php";
-        		        }
-    		        }
-		        }else{
-					$visiting_page = $home_page;
-		        }
+		if (verify_access($db, $encryption)){
+			if (isset($_COOKIE[$login_cookie])){
+				$uid = $_COOKIE[$login_cookie];
+				if ($uid > 0){
+					// reset cookie length
+					setcookie($login_cookie, $uid, $cookie_time);
+					$user = $db->getRow("fh_users", "id = '" . $uid . "'");
+					$userAccessLevel = $user['priviledge_id'];
+					$accessCheck = $db->getRow("fh_priviledge_settings", "priviledge_id = '". $userAccessLevel . "'");
+					if (isset($_GET['page']) && is_array($accessCheck)){
+						if (array_key_exists($_GET['page'], $accessCheck)){
+							// check priviledges to see if user can have this page shown or not
+							// if not divert away to home page (defined in the settings)
+							if ($_GET['page'] != "logout"){
+								if ($accessCheck[$_GET['page']] == 1){
+								   //Authorised
+									$visiting_page = $_GET['page'] . ".php";
+								}else{
+								   // Unauthorised 
+								   $visiting_page = "unauthorised_access.php";
+								}
+							}else{
+								$visiting_page = $home_page;
+							}
+						}else{
+							if ($_GET['page'] != "logout"){
+								// page doesnt exist
+								$visiting_page = "404.php";
+							}else{
+								$visiting_page = "logout.php";
+							}
+						}
+					}else{
+						$visiting_page = $home_page;
+					}
+				}else{
+					if (isset($_GET['page'])){
+						if($_GET['page'] != ""){
+							$visiting_page =  $_GET['page'] . ".php";
+						}else{
+							$visiting_page = $home_page;
+						}
+					}else{
+						$visiting_page = $home_page;
+					}
+				}
 			}else{
-			    if (isset($_GET['page'])){
-			        if($_GET['page'] != ""){
-    			        $visiting_page =  $_GET['page'] . ".php";
-			        }else{
-        			    $visiting_page = $home_page;
-			        }
-			    }else{
-	        	    $visiting_page = $home_page;
-			    }
+				$user = NULL;
+				if (isset($_GET['page'])){
+					if($_GET['page'] != ""){
+						$visiting_page =  $_GET['page'] . ".php";
+					}else{
+						$visiting_page = $home_page;
+					}
+				}else{
+					$visiting_page = $home_page;
+				}
 			}
 		}else{
-			$user = NULL;
-            if (isset($_GET['page'])){
-		        if($_GET['page'] != ""){
-			        $visiting_page =  $_GET['page'] . ".php";
-		        }else{
-    			    $visiting_page = $home_page;
-		        }
-		    }else{
-        	    $visiting_page = $home_page;
-		    }
+			setcookie($login_cookie, "", time()-3600);
+			setcookie($session_code, "", time()-3600);
+			$visiting_page = $home_page;
 		}
-	}else{
-		$visiting_page = $inc_dir . "no_db.php";	
-	}
-
+    }else{
+        $visiting_page = $home;
+    }
 ?>
