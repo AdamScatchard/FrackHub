5f595a6d3487d4f601c127e33d46af8d3a1c8c54
WLVCollabTeam1 <99879568+WLVCollabTeam1@users.noreply.github.com>
2022-04-11 16:37:16 +0100

    Update settings.php
    new global setting for login applied

--- settings.php
+++ settings.php
@@ -1,44 +1,32 @@
 <?php
-
 $settings = true;
-
 $debug = true;
 
-
-
 // Database settings
-
 $db_sn = "id18498157_frackhubtdb";
-
 $db_pwd = "Bm1b*2F3m!*oy0j0";
-
 $db_db = "id18498157_frackhubtestingdb";
-
 $db_host = "localhost";
-
 $encryption_key = 12;
 
 // Library and Dependant Folders
 
 $lib_dir = "libs/";
-
 $css_dir = "css/";
-
 $js_dir = "js/";
-
 $inc_dir = "inc/";
 
-
-
 $home_page = "home_page.php";
-
-
 
 // login expire
 
 $cookie_time = time() + 3600;
 $login_cookie = "frackhub";
+$session_code = "fh_session";
+$temporary_lock_time = 900;     // setting implemented for Saxons security settings (Adam MacKay)
 
+// time to delete inactive accounts
+$expire_accounts = 15811200;
 
 // Email Settings (Future use for email validation)
 
