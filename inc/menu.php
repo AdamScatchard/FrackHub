34b04576c9ce72cefdfd1e0a671afaa34b1577d0
WLVCollabTeam1 <99879568+WLVCollabTeam1@users.noreply.github.com>
2022-04-11 16:38:22 +0100

    Update menu.php
    Menu changed to include the Search page

--- inc/menu.php
+++ inc/menu.php
@@ -1,35 +1,23 @@
 <div class="navbar">
-
 	<div class="logo_div">
-
 		<a href="index.php"><h1>FrackHub</h1></a>
-
-	</div>
-	
-	<ul>
-
+	</div>		<ul>
 	<?php 	
-		if (isset($uid)){
-			echo '<li><a class="active" href="?page=account">Account</a></li>
-			<li><a class="active" href="?page=advertise">Advertise</a></li>
-			<li><a class="active" href="?page=book_item">Book Item</a></li>';
-		}else{
-			echo '<li><a class="active" href="index.php">Home</a></li>';
+		if (isset($uid)){			
+		    echo "<li><a class=\"active\" href=\"?page=account\">Account</a></li>";
+		    echo "<li><a class=\"active\" href=\"?page=advertise\">Advertise</a></li>";
+		    echo "<li><a class=\"active\" href=\"?page=book_item\">Book Item</a></li>";
+		}else{			
+		    echo "<li><a class=\"active\" href=\"index.php\">Home</a></li>";
 		}
 	?>
-
-	<li><a href="#news">Search</a></li>
-
+	<li><a href="index.php?page=search">Search</a></li>
 	<li><a href="#news">Message</a></li>
-
-	<li><a href="#contact">Contact</a></li>
-
-	<?php
-		if (isset($uid)){
-			echo '<li><a href="?page=logout">Logout</a></li>';
-		}
+	<li><a href="#contact">Contact</a></li>	
+	<?php		
+	    if (isset($uid)){
+	        echo '<li><a href="?page=logout">Logout</a></li>';		
+	    }	
 	?>
-
 	</ul>
-
 </div>
