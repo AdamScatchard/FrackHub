f2fa8b620514380d1c7fdf7e16d2b69d6e6b1890
WLVCollabTeam1 <99879568+WLVCollabTeam1@users.noreply.github.com>
2022-04-18 19:40:25 +0100

    Update luhn.js

--- js/luhn.js
+++ js/luhn.js
@@ -9,23 +9,21 @@
 // perform modulous 10.
 
 function validateCard(strCardNumber){
-  // check the card number is solely numbers.
 	if (isNaN(strCardNumber)){
 		console.log ("Card number contains non-numerical characters");
 		return false;
+	}else{
+	    if ((parseInt(strCardNumber) % 10)==0){
+	        return false;
+	    }
+	    if(strCardNumber.length < 14){
+	        return false;
+	    }
 	}
-  // initiate integer variable sum
 	sum = 0;
-  // split the card number into an array called longNumber
 	longNumber = strCardNumber.split('');
-  // Capture the last digit of the array as the check digit and remove it
-  // from the original array (longNumber)
 	chkDigit = parseInt(longNumber.pop());
-  
-  // itenerate the longNumber array items
 	for (i = 0; i < longNumber.length; i++){
-    // if i is an even nth position then double the digit and (add the digits together if
-    // the value is greater or equals to 10), otherwise simply keep variable x as the single digit
 		if (i % 2 == 0){
 			evens = (parseInt(longNumber[i]) * 2);
 			if (evens >= 10){
@@ -35,18 +33,13 @@ function validateCard(strCardNumber){
 				x = evens;
 			}
 		}else{
-      // not an even nth position, simply copy the value to variable x
 			x = longNumber[i];
 		}
-    // sum up the value of variable sum with the new value of x
 		sum+= parseInt(x);
 	}
-  // add the chkDigit value to the sum 
 	sum+=chkDigit;
-  // if divisible by 10 then the card passes luhn algorithm
 	if (sum % 10 == 0){
 		return true;
 	}
-  // otherwise the card number doesnt check out
 	return false;
 }
