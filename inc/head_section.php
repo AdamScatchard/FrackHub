c13ccc92a0f232b0f3684eb9b8165f1d85f1db15
WLVCollabTeam1 <99879568+WLVCollabTeam1@users.noreply.github.com>
2022-04-18 19:38:13 +0100

    Update head_section.php

--- inc/head_section.php
+++ inc/head_section.php
@@ -1,19 +1,41 @@
 <?php
 	echo "<!DOCTYPE html>";
-
 	echo "<html>";
-
 	echo "<head>";
-
 	echo "<link href=\"https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine\" rel=\"stylesheet\">";
 	// modified by adam mackay as it should have $css_dir define the css path.
 	// in the future the HTML title tag will contain data to show the page title.
-
 	echo "<link rel=\"stylesheet\" href=\"" . $css_dir . "page_formatting.css\">";
 	echo "<meta charset=\"UTF-8\">";
 
 	// page title needs adding here
-	echo "<title>This should be update by PHP</title>";
+	if (isset($uid)){
+    	if ($uid > 0){
+    	    if (isset($_GET['page'])){
+        	    switch(strtolower($_GET['page'])){
+        	        case "registration":
+        	            $title = "Frackhub: Registration";
+        	            break;
+        	         case "account":
+        	             $title = "Frackhub: Account details";
+        	             break;
+                     case "logout":
+                         $title = "Frackhub: Logged out";
+                         break;
+                    default:
+                        $title = "Welcome back " . $user['name'] . " to FrackHub";
+                        break;
+        	    }
+    	    }else{
+                $title = "Welcome to Frackhub";
+    	    }
+        	echo "<title>" .$title ."</title>";
+    	}else{
+    	    echo "<title>Welcome to Frackhub</title>";
+    	}
+	}else{
+    	    echo "<title>Welcome to Frackhub</title>";
+	}
 	echo "</head>";
-	echo "<body>"; 
+	echo "<body>";
 ?>
