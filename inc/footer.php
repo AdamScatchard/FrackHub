3ce816806baa8ee969bc0bf76d6881cd4e5fe4c5
WLVCollabTeam1 <99879568+WLVCollabTeam1@users.noreply.github.com>
2022-04-30 00:41:44 +0100

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,33 @@
 <?php
-	echo "<div class=\"footer\">";
-	echo "-- FrackHub Collaborative Development 5CS024 -- &copy;" . date('Y') . "";
-	echo "<div>";
+    echo "</main>";
+	echo "<footer>";
+    echo "<table>";
+    echo "<tr>";
+    echo "<td>";
+    
+    echo "Frackhub<br>";
+    echo "Wolverhampton University<br>";
+    echo "Wulfruna Street<br>";
+    echo "Wolverhampton<br>";
+    echo "England<br>";
+    echo "WV1 1LY";
+    echo "</td>";
+    echo "<td>";
+    echo "<ul class='nobullets'>";
+    echo "<li>Site Map</li>";
+    echo "<li>About Us</li>";
+    echo "<li>Press</li>";
+    echo "<li>Career Opportunities</li>";
+    echo "<li>Contact Us</li>";
+
+    echo "</ul>";
+    echo "</td>";
+    echo "<td>";
+    echo "Company Number: 2661891728";
+    echo "</td>";
+    echo "</tr>";
+    echo "</table>";
+	echo "FrackHub Collaborative Development 5CS024 &copy;Copyright all rights reserved" . date('Y') . "";
+	echo "</footer>";
 	echo "</html>";
 ?>
