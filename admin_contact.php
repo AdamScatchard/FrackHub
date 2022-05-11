08ccc0aa8230b7f39ed0331d3d8fe450f15b94f2
WLVCollabTeam1 <99879568+WLVCollabTeam1@users.noreply.github.com>
2022-05-11 15:17:55 +0100

    Update admin_contact.php
    Update contains data from the database on what emails have been received and also split the table and div for better readability

--- admin_contact.php
+++ admin_contact.php
@@ -4,6 +4,46 @@
     echo "<hr>";
     $adverts = $db->query("fh_contactus", null, "viewed=0");
     echo "<div class='contactContainer'>";
+    echo "<h2>Unread Messages</h2>";
+    if ($adverts){
+        echo "<table>";
+        echo "<tr>";
+        echo "<th>Time Date</th>";
+        echo "<th>Name</th>";
+        echo "<th>E-Mail</th>";
+        echo "<th>Subject</th>";
+        echo "<th>Senders IP</th>";
+        echo "<th>View</th>";
+        echo "</tr>";
+        foreach ($adverts as $email){
+            echo "<tr>";
+            echo "<td>";
+            echo date("H:i d/m/Y", $email['timestamp']);
+            echo "</td>";
+            echo "<td>";
+            echo $email['name'];
+            echo "</td>";
+            echo "<td>";
+            echo $email['email'];
+            echo "</td>";
+            echo "<td>";
+            echo $email['subject'];
+            echo "</td>";
+            echo "<td>";
+            echo $email['ip'];
+            echo "</td>";
+            echo "<td>";
+            echo "<a href='index.php?page=admin_reply&id=" . $email['id'] . "'>View Email</a>";
+            echo "</td>";
+            echo "</tr>";
+    
+        }
+        echo "</table>";
+    }else{
+        echo "No unread messages";
+    }
+    echo "<h2>Read Emails</h2>";
+    $adverts = $db->query("fh_contactus", null, "viewed=1");
     echo "<table>";
     echo "<tr>";
     echo "<th>Time Date</th>";
@@ -36,5 +76,6 @@
         echo "</tr>";
 
     }
-    echo "</table></div>";
+    echo "</table>";
+    echo "</div>";
 ?>
