a760fe670d9a2b42d52fd918018eb482f8a1705b
WLVCollabTeam1 <99879568+WLVCollabTeam1@users.noreply.github.com>
2022-05-11 15:31:55 +0100

    Update index.php
    Changes to allow for Banned IP addresses

--- index.php
+++ index.php
@@ -5,9 +5,14 @@
 	require_once($inc_dir . 'head_section.php');
 	
 	include ($inc_dir . 'menu.php');
-
-	include ($visiting_page);
-	
+    $banned = $db->getRow("fh_banned_connections", "user_ip ='" . $_SERVER['REMOTE_ADDR'] . "'");
+    if ($banned){
+        echo "<h2>Banned Connection</h2>";
+        echo "<hr>";
+        echo "<p>Your connection " . $_SERVER['REMOTE_ADDR'] . " is banned from our services</p>";
+    }else{
+    	include ($visiting_page);
+    }
 	include($inc_dir . 'footer.php');
 	$db->disconnect();
 	
