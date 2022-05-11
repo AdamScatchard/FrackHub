fc6af2b4c2ac1274f3820e1857ad333a24f9a777
WLVCollabTeam1 <99879568+WLVCollabTeam1@users.noreply.github.com>
2022-05-11 15:48:49 +0100

    Update search.php
    Modifications for mobile / tablet view (searchBtn_x) as this is an input type image
    Bug fix: show adverts active, as newly created adverts could be searched for
    Restrict how many characters are shown in the search page (trim based on a size set in settings.php) this is given that adverts can be of any length, and would disrupt the search page appearance

--- search.php
+++ search.php
@@ -6,21 +6,21 @@
     echo "<h2>Results:</h2>";
     echo "<hr>";
     echo "<div class='contactContainer'>";
-    if (isset($_POST["searchBtn"])) {
-    	$str = $_POST["search_text"];
+    if (isset($_POST["searchBtn"]) || isset($_POST["searchBtn_x"])) {
+    	$str = $db->cleanSQLInjection($_POST["search_text"]);
     	if (($str != null) && ($str != "")){ 
-        	$res = $db->query("fh_adverts", NULL, "name LIKE '%" . $str . "%' AND available = 1");
+        	$res = $db->query("fh_adverts", NULL, "name LIKE '%" . $str . "%' AND active='1' and available >0");
             if (count($res) > 0){
                 echo "<table><tr><th>Name</th><th>Description</th></tr>";
                 foreach ($res as $result){
                     echo "<tr>";
-                    echo "<td><a href=\"index.php?page=item&view_item=" . $result['id'] . "\">" . $result['name'] . "</a></td>";
-                    echo "<td>" . $result['description'] . "</td>";
+                    echo "<td class='search_results_line'><a href=\"index.php?page=item&view_item=" . $result['id'] . "\">" . $result['name'] . "</a></td>";
+                    echo "<td class='search_results_line'>" . substr($result['description'],0,(strval($search_description_char_size)-1)) . "</td>";
                     echo "</tr>";
                 }
                 echo "</table>";
             }else{
-                echo "<h1>No results located for: '" . $str . "'</h1>";
+                echo "<h2>No results located for: '" . $str . "'</h2>";
             }
     	}else{
     	    echo "<h3>What is it you are looking for?</h3>";
