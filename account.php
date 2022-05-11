35a519148e7469003c585917d2d199ffe9cdf2bc
WLVCollabTeam1 <99879568+WLVCollabTeam1@users.noreply.github.com>
2022-05-11 15:12:20 +0100

    Update account.php
    Changes applied
    Verify close account 
    Cleaning SQL Injection 
    Link to Full Statement

--- account.php
+++ account.php
@@ -43,11 +43,30 @@
     
     // Close an account (Adam M)
     if (isset($_POST['close'])){
-		$db->update("fh_users", ["active"=>0], "id=" . $uid);
-		setcookie($login_cookie, "", time()-3600);
-		setcookie($session_code, "", time()-3600);
-		echo "<h1>Your account has been closed down</h1>";
-		die();
+        if (isset($_POST['closePassword'])){
+            $user = $db->getRow("fh_users", "id='" . $uid . "'");
+            $encryption->setPlainText($_POST['closePassword'] . $user["username"] . $user["email"]. $user["timestamp"]);
+            $pwdEntered = $encryption->classRun();
+            if ($pwdEntered == $user['password']){
+        		$db->update("fh_users", ["active"=>0], "id=" . $uid);
+        		setcookie($login_cookie, "", time()-3600);
+        		setcookie($session_code, "", time()-3600);
+        		echo "<h1>Your account has been closed down</h1>";
+        		die();
+            }else{
+        		echo "<h1>Your password supplied was incorrect</h1>";
+        		echo "<p>As a protective meassure, we have logged you out, if you still wish to close your account<br>";
+        		echo "please log back in and enter the correct password associated with this account.</p>";
+        		setcookie($login_cookie, "", time()-3600);
+        		setcookie($session_code, "", time()-3600);
+                die();                
+            }
+        }
+        // check password
+        // encrypt it
+        // if match do this
+		// else
+		// massage close failed.
 	}
 	
 	//Edit Account Info (Adam M)
@@ -57,21 +76,21 @@
     		    switch ($key){
     		        case "dob":
     		            if ($value != NULL){
-    		                $value = trim(strtotime($value));
+    		                $value = $db->cleanSQLInjection(trim(strtotime($value)));
     		            }else{
     		                $value = 0;
     		            }
-		                $database_values[$key] = $value;
+		                $database_values[$key] = $db->cleanSQLInjection($value);
 		                break;
 		            case "password":
 						if ($value != ""){
-        				    $encryption->setPlainText($value . $_POST["username"] . $_POST["email"] . time());
+        				    $encryption->setPlainText($db->cleanSQLInjection($value) . $db->cleanSQLInjection($_POST["username"]) . $db->cleanSQLInjection($_POST["email"]) . time());
         				    $value = $encryption->classRun();
         				    $database_values[$key] = $value;
 						}
 		                break;
     		        default:
-		                $database_values[$key] = $value;
+		                $database_values[$key] = $db->cleanSQLInjection($value);
     		    }
     		}
 	    }
@@ -85,48 +104,59 @@
 
     // return items    
     if(isset($_POST['return_item'])){
-        $transaction = $db->getRow("fh_items_loaned", "id='" . $_POST['return_item'] . "'");
-        $advert = $db->getRow("fh_adverts", "id='" . $transaction['itemID'] . "'");
-        $returnedAmt = $advert['amount_available'] + $transaction['amount_loaned'];
-        // calculate credits charge
-        // calculate days
-        $days = intval((time() - $transaction['timestamp']) / 86400) + 1;
-        $charge = ($advert['credits'] * $transaction['amount_loaned']) * $days;
-        if ($credits > $charge){
-            // Loaners Balance Changes
-            $credits -= $charge;
-            $newData['userID'] =$uid;
-            $newData['advertID']=$advert['id'];
-            $newData['transactionType']=1;
-            $newData['ip_address'] = $_SERVER['REMOTE_ADDR'];
-            $newData['timestamp'] = time();
-            $newData['balance'] = $credits;
-            $newData['changeCredit'] = $charge;
-            $newData['viewed'] = 0;
-            $db->insert("fh_ledger", $newData);
-            $returned = $db->update("fh_adverts", ['amount_available' => $returnedAmt, 'available'=>1], "id='" . $advert['id'] . "'" );
-            $returned = $db->delete("fh_items_loaned", "id=" . $transaction['id']);
-            
-            // now top up the loaners account
-            $utp = $db->query("fh_ledger", null, "userID='" . $advert['userID'] . "'", false, ['timestamp' => "DESC"], 1);
-            // credits
-            $newData['userID'] =$advert['userID'];
-            $newData['advertID']=$advert['id'];
-            $newData['transactionType']=2;
-            $newData['ip_address'] = $_SERVER['REMOTE_ADDR'];
-            $newData['balance'] = $utp[0]['balance'] += $charge;
-            $newData['changeCredit'] = $charge;
-            $newData['viewed'] = 0;
-            $db->insert("fh_ledger", $newData);
-
-            if ($returned){
-                echo "Item returned";
-            }else{
-                echo "There has been an issue returning this item";
+        $transaction = $db->getRow("fh_items_loaned", "id='" . $db->cleanSQLInjection($_POST['return_item']) . "'");
+        if ($transaction){
+            if ($transaction['loanerID'] == $uid){
+                // user has loaded this item and can return it (No else condition as we just ignore it if the user)
+                // has messed with the source code in developer mode.
+                $advert = $db->getRow("fh_adverts", "id='" . $transaction['itemID'] . "'");
+                $returnedAmt = $advert['amount_available'] + $transaction['amount_loaned'];
+                // calculate credits charge
+                // calculate days
+                $days = intval((time() - $transaction['timestamp']) / 86400) + 1;
+                $charge = ($advert['credits'] * $transaction['amount_loaned']) * $days;
+                if ($credits > $charge){
+                    // Loaners Balance Changes
+                    $credits -= $charge;
+                    $newData['userID'] =$uid;
+                    $newData['advertID']=$advert['id'];
+                    $newData['transactionType']=1;
+                    $newData['ip_address'] = $_SERVER['REMOTE_ADDR'];
+                    $newData['timestamp'] = time();
+                    $newData['balance'] = $credits;
+                    $newData['changeCredit'] = $charge;
+                    $newData['viewed'] = 0;
+                    $db->insert("fh_ledger", $newData);
+                    $returned = $db->update("fh_adverts", ['amount_available' => $returnedAmt, 'available'=>1], "id='" . $advert['id'] . "'" );
+                    $returned = $db->delete("fh_items_loaned", "id=" . $transaction['id']);
+                    
+                    // now top up the loaners account
+                    $utp = $db->query("fh_ledger", null, "userID='" . $advert['userID'] . "'", false, ['timestamp' => "DESC"], 1);
+                    // credits
+                    
+                    $newData['userID'] =$advert['userID'];
+                    $newData['advertID']=$advert['id'];
+                    $newData['transactionType']=2;
+                    $newData['ip_address'] = $_SERVER['REMOTE_ADDR'];
+                    if ($utp){
+                        $newData['balance'] = $utp[0]['balance'] += $charge;
+                    }else{
+                        $newData['balance'] = $charge;
+                    }
+                    $newData['changeCredit'] = $charge;
+                    $newData['viewed'] = 0;
+                    $db->insert("fh_ledger", $newData);
+                    
+                    if ($returned){
+                        echo "<h3>Item recorded as returned</h3>";
+                    }else{
+                        echo "<h3>There has been an issue returning this item</h3>";
+                    }
+                
+                }else{
+                    echo "<h2>You have insufficient credits to return this item</h2>";
+                }
             }
-         
-        }else{
-            echo "You have insufficient credits to return this item";
         }
 	}
     
@@ -134,7 +164,7 @@
     if(isset($_POST['top_up_submit']) && isset($_POST['top_up_credits'])){
 		if($_POST['top_up_credits'] > 0){
             include ($lib_dir . "luhn_checker.php");
-            $cardCheck = checkLuhnCardNumber($_POST['cardnumber']);
+            $cardCheck = $_POST['cardnumber'];
             if ($cardCheck == false){
                 echo "<h1>Failed Luhn Card Verification</h1>";
                 die();
@@ -142,9 +172,9 @@
 
 		    $latest_transaction = $db->query("fh_ledger", ["balance"], "userID = '" . $uid . "'", false, ["timestamp" => "DESC"], 1);
 			$old_balance = $latest_transaction? $latest_transaction[0]["balance"] : 0;
-			$new_balance = $old_balance + $_POST['top_up_credits'];
+			$new_balance = $old_balance + intval($_POST['top_up_credits']);
 			$database_values = ["userID" => $uid, "advertID" => -1, "TransactionType" => 0, "ip_address" => $_SERVER['REMOTE_ADDR'],
-			"timestamp" => time(), "viewed" => 0, "balance" => $new_balance, "changeCredit" => $_POST['top_up_credits']];
+			"timestamp" => time(), "viewed" => 0, "balance" => $new_balance, "changeCredit" => $db->cleanSQLInjection($_POST['top_up_credits'])];
 			
 			$result = transaction($database_values, $old_balance);
 			
@@ -180,7 +210,7 @@
     echo "<tr><td>DoB:</td><td><input type='date' id='dob' name='dob' value='" .  date('Y-m-d',$user['dob']) . "' onblur='ageVerification(this, \"dobSign\");'><span id='dobSign'></span></td></tr>";
     echo "</table>";
     echo "<input type='submit' id='updateAcc' value='Submit' name='submit' class='btn' >";
-    echo "<input type='submit' value='Close Account' name='close' class='btn'>";
+    echo "<input type='button' value='Close Account' onclick='verifyCloseAccount(true);' class='btn'>";
     echo "</form>";
     echo "</div>";
     
@@ -215,8 +245,8 @@
 	for ($i = (date("Y") - 4); $i <= (date("Y") + 4); $i++){
 	    echo "<option value=" . $i . ">" . $i . "</option>";
 	}
-	echo "</select></td></tr>";
-	echo '<td><input type = "submit" name = "top_up_submit"  id = "topup" value = "Top Up" class = "btn" disabled=disabled></td>';
+	echo "</select></td></tr><tr>";
+	echo '<td colspan=2><input type = "submit" name = "top_up_submit"  id = "topup" value = "Top Up" class = "btn" disabled=disabled></td>';
 
 
 	echo '</tr>';
@@ -250,7 +280,7 @@
 
 		}
         echo "</table>";
-        echo "<p><a href='#'>more...</a>";
+        echo "<p><a href='index.php?page=creditstatement'>more...</a>";
 	}else{
 	    echo "<h2>No items</h2>";
 	}
@@ -340,7 +370,18 @@
         echo "<h2>You do not have any borrowed items</h2>";
     }
     echo "</div>";    
-echo "</div>";    
+echo "</div>"; 
+echo "<div id='closeAccScreen'>";
+echo "<form id='closeForm' name='registrationform' method='post' action='index.php?page=account'>";
+echo "<h1>Close Account Confirmation</h1>";
+echo "<h3>You are about to close your account down</h3>";
+echo "<p>To proceed enter your password and then select 'Close My Account'<br><br>";
+echo "<input type='password' name='closePassword' placeholder='verify password'><br><br>";
+echo "<input type='submit' class='btn' value='Close My Account' name='close'>";
+echo "<input type='button' class='btn' value='Cancel' onclick='verifyCloseAccount(false)'>";
+echo "</p>";
+echo "</form>";
+echo "</div>";
 
 ?>
 
@@ -408,4 +449,15 @@ echo "</div>";
         checks['dob'] = age;
         passfailDisplay(fieldsSgn, age);
     }
+	function verifyCloseAccount(show){
+    	el = document.getElementById("closeAccScreen");
+    	if (show){
+    		el.style.height = window.innerHeight + "px";
+    		el.style.width = window.innerWidth + "px";
+    		el.style.display = "block";
+    	}else{
+    		el.style.display = "none";
+    	}
+	}
+
 </script>
