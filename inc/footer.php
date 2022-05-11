b1528e2219de654d8b31415a36252c5d027a2af5
WLVCollabTeam1 <99879568+WLVCollabTeam1@users.noreply.github.com>
2022-05-11 15:55:40 +0100

    Update footer.php
    Mobile phone / tablet view changes

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,7 @@
 <?php
     echo "</main>";
 	echo "<footer>";
-    echo "<table>";
+    echo "<table id='desktopFooter'>";
     echo "<tr>";
     echo "<td>";
     
@@ -27,7 +27,22 @@
     echo "</td>";
     echo "</tr>";
     echo "</table>";
-	echo "FrackHub Collaborative Development 5CS024 &copy;Copyright all rights reserved" . date('Y') . "";
+	echo "FrackHub Collaborative Development 5CS024 &copy;Copyright all rights reserved " . date('Y') . "";
 	echo "</footer>";
+	// script to make the animated menu in mobile view
+	echo "<script>";
+	echo "function myFunction(x) {";
+    echo "  x.classList.toggle('change');";
+    echo "  el = document.getElementById('mobileSearchBar');";
+    echo "  el2 = document.getElementById('mobileMenuBts');";
+    echo "  if (el.style.display === 'none') {";
+    echo "       el.style.display = 'inline-block';";
+    echo "       el2.style.display = 'none';";
+    echo "  } else {";
+    echo "       el.style.display = 'none';";
+    echo "       el2.style.display = 'inline-block';";
+    echo "  }";
+    echo "}";
+	echo "</script>";
 	echo "</html>";
 ?>
