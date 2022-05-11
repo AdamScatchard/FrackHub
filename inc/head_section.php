173b890c8f0e6d17f9299fd444b519a8f643188b
WLVCollabTeam1 <99879568+WLVCollabTeam1@users.noreply.github.com>
2022-05-11 15:56:28 +0100

    Update head_section.php
    HTML Validation changes
    Mobile phone and tablet view port added

--- inc/head_section.php
+++ inc/head_section.php
@@ -1,9 +1,9 @@
 <?php
 	echo "<!DOCTYPE html>";
-	echo "<html>";
+	echo "<html lang='en'>";
 	echo "<head>";
-	echo "<link href=\"https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine\" rel=\"stylesheet\">";
-	// modified by adam mackay as it should have $css_dir define the css path.
+    echo "<meta name='viewport' content='width=device-width, initial-scale=1'>";
+		// modified by adam mackay as it should have $css_dir define the css path.
 	// in the future the HTML title tag will contain data to show the page title.
 	echo "<link rel=\"stylesheet\" href=\"" . $css_dir . "page_formatting.css\">";
 	echo "<meta charset=\"UTF-8\">";
@@ -38,4 +38,4 @@
 	}
 	echo "</head>";
 	echo "<body>";
-?>	
+?>
